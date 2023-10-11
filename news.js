437b0296b414a20be95dbe20625ca426858c776b
Ahmedhail <aboazooz.hail@gmail.com>
2023-10-11 18:10:32 +0300

    add index

---
+++ news.js
@@ -0,0 +1,11 @@
+let menue=document.querySelector(".top-right img")
+let top_right=document.querySelector(".top-right ")
+
+console.log(menue)
+console.log(top_right)
+
+
+menue.addEventListener("click",()=>{
+    top_right.classList.toggle("on")
+})
+    
